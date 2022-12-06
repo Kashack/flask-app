05be411cfa8d1a71016f4900e99451f29e11cc4c
Elias <kasumu.elias@gmail.com>
2022-12-06 14:35:24 +0100

    change

--- app.py
+++ app.py
@@ -5,17 +5,17 @@ import numpy as np
 app = Flask(__name__)
 model = pickle.load(open('model.pkl','rb'))
 
-@app.route('/predict',methods=['GET'])
+@app.route('/', methods=['GET'])
 
 def predict():
     # input_data = (60182,0,49,0,1,2,1,171.23,34.4,3,1)
-    input_data = request.get_json(force=True)
+    # input_data = request.get_json(force=True)
     # input_data_as_numpy_array = np.asarray(input_data)
     # input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
-    prediction = model.predict(input_data)
-
+    # prediction = model.predict(input_data)
+    data = request.form.to_dict()
     # data = request.get_json(force=True)
-    # prediction = model.predict([[np.array(data['exp'])]])
+    prediction = model.predict([[np.array(data['exp'])]])
     output = prediction[0]
 
     if(output == 0):
